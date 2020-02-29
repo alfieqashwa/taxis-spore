374fae372799b7e45e83ced5c681ad1560749736
alfieqashwa <alfieqashwa@gmail.com>
2020-03-01 01:02:58 +0700

    added Loading && Error handling

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 import Map from "./Map";
@@ -9,16 +9,25 @@ const API = "https://api.data.gov.sg/v1/transport/taxi-availability";
 const initialQuery = "2020-02-20T20%3A20%3A00Z";
 
 function App() {
-  const [data, setData] = React.useState([]);
-  const [total, setTotal] = React.useState(0);
-  const [startDate, setStartDate] = React.useState(new Date());
-  const [query, setQuery] = React.useState(initialQuery);
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [total, setTotal] = useState(0);
+  const [startDate, setStartDate] = useState(new Date());
+  const [query, setQuery] = useState(initialQuery);
 
   React.useEffect(() => {
     const fetchData = async () => {
-      const result = await axios(`${API}?date_time=${query}`);
-      setData(result.data.features[0].geometry.coordinates);
-      setTotal(result.data.features[0].properties.taxi_count);
+      setIsError(false);
+      setIsLoading(true);
+      try {
+        const result = await axios(`${API}?date_time=${query}`);
+        setData(result.data.features[0].geometry.coordinates);
+        setTotal(result.data.features[0].properties.taxi_count);
+      } catch (error) {
+        setIsError(true);
+      }
+      setIsLoading(false);
     };
     fetchData();
   }, [query]);
@@ -38,8 +47,11 @@ function App() {
         startDate={startDate}
         onChange={date => setStartDate(date)}
         onSubmit={onSubmit}
+        isLoading={isLoading}
+        isError={isError}
       />
     </div>
   );
 }
+
 export default App;
--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -1,11 +1,23 @@
 import React from "react";
 import DatePicker from "react-datepicker";
 
-function ControlPanel({ total, startDate, onChange, onSubmit }) {
+function ControlPanel({
+  total,
+  startDate,
+  onChange,
+  onSubmit,
+  isLoading,
+  isError
+}) {
   return (
     <div className="control-panel">
       <h3>Taxis Availability</h3>
-      <h4>Total Taxi: {total}</h4>
+      <h4>Country: Singapore</h4>
+      {isLoading ? (
+        <h4 style={{ color: "tomato" }}>Loading...</h4>
+      ) : (
+        <h4>Total Taxi: {total}</h4>
+      )}
       <form onSubmit={onSubmit}>
         <DatePicker
           selected={startDate}
@@ -19,6 +31,12 @@ function ControlPanel({ total, startDate, onChange, onSubmit }) {
         />
         <button type="submit">Submit Date</button>
       </form>
+      {isError && (
+        <div style={{ textAlign: "center", color: "red" }}>
+          <h2>Something went wrong ...</h2>
+          <p>Please input between 2016 to today!</p>
+        </div>
+      )}
     </div>
   );
 }
